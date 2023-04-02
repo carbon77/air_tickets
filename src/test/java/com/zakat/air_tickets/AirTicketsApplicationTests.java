80409a4bb0366bedb6828a66744c504701e2fbac
Igor <42415653+carbon77@users.noreply.github.com>
2023-04-02 17:06:09 +0300

    Initial commit

---
+++ src/test/java/com/zakat/air_tickets/AirTicketsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zakat.air_tickets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirTicketsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
