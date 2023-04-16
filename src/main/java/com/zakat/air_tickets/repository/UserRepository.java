2312d4f50da38943707eb1e04828b62a0cda14e9
Igor <zakatov0303@gmail.com>
2023-04-16 15:31:13 +0300

    Fixed authentication classes
    Added findByUsername method to repository and service

--- src/main/java/com/zakat/air_tickets/repository/UserRepository.java
+++ src/main/java/com/zakat/air_tickets/repository/UserRepository.java
@@ -4,4 +4,5 @@ import com.zakat.air_tickets.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
+    User findByUsername(String username);
 }
--- src/main/java/com/zakat/air_tickets/security/SecurityService.java
+++ src/main/java/com/zakat/air_tickets/security/SecurityService.java
@@ -1,20 +1,66 @@
 package com.zakat.air_tickets.security;
 
+import com.vaadin.flow.component.UI;
+import com.vaadin.flow.server.VaadinServletRequest;
 import com.vaadin.flow.spring.security.AuthenticationContext;
+import com.zakat.air_tickets.entity.User;
+import com.zakat.air_tickets.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class SecurityService {
+public class SecurityService implements UserDetailsService {
 
-    private final AuthenticationContext authenticationContext;
+    private final String LOGIN_SUCCESS_URL = "/";
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    public UserPrincipal getAuthenticatedUser() {
-        return authenticationContext.getAuthenticatedUser(UserPrincipal.class).get();
+    public UserDetails getAuthenticatedUser() {
+        SecurityContext securityContext = SecurityContextHolder.getContext();
+        Object principal = securityContext.getAuthentication().getPrincipal();
+
+        if (principal instanceof UserPrincipal) {
+            return (UserDetails) principal;
+        }
+
+        return null;
+    }
+
+    public User createUser(String username, String email, String password) {
+        String encodedPassword = passwordEncoder.encode(password);
+        User user = new User();
+        user.setPassword(encodedPassword);
+        user.setEmail(email);
+        user.setUsername(username);
+
+        return userRepository.save(user);
     }
 
     public void logout() {
-        authenticationContext.logout();
+        UI.getCurrent().getPage().setLocation(LOGIN_SUCCESS_URL);
+        SecurityContextLogoutHandler logoutHandler = new SecurityContextLogoutHandler();
+        logoutHandler.logout(
+                VaadinServletRequest.getCurrent().getHttpServletRequest(), null,
+                null
+        );
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username);
+
+        if (user == null) {
+            throw new UsernameNotFoundException(username);
+        }
+
+        return new UserPrincipal(user);
     }
 }
