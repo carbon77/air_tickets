59c00890311d14f9c27b7ca87a751536986a2887
Igor <zakatov0303@gmail.com>
2023-04-16 15:33:05 +0300

    Fixed RegisterView and LoginView and created MainView
    Wrong basic layout usage

--- src/main/java/com/zakat/air_tickets/view/LoginView.java
+++ src/main/java/com/zakat/air_tickets/view/LoginView.java
@@ -1,51 +1,32 @@
 package com.zakat.air_tickets.view;
 
-import com.vaadin.flow.component.ClickEvent;
-import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.button.ButtonVariant;
-import com.vaadin.flow.component.html.H2;
-import com.vaadin.flow.component.icon.Icon;
-import com.vaadin.flow.component.icon.VaadinIcon;
-import com.vaadin.flow.component.orderedlayout.FlexComponent;
+import com.vaadin.flow.component.login.LoginForm;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
+import com.vaadin.flow.router.RouteAlias;
 import com.vaadin.flow.server.auth.AnonymousAllowed;
 import com.zakat.air_tickets.components.HeaderLayout;
-import com.zakat.air_tickets.components.LoginForm;
-import com.zakat.air_tickets.entity.User;
 
-@Route("/login")
+@Route(value = "/login", layout = HeaderLayout.class)
+@RouteAlias("login")
 @PageTitle("SkyWing | Login")
 @AnonymousAllowed
-public class LoginView extends HeaderLayout {
+public class LoginView extends VerticalLayout {
     private final LoginForm loginForm;
 
     public LoginView() {
-        VerticalLayout layout = new VerticalLayout();
-        layout.setWidth("30%");
-        layout.setAlignItems(FlexComponent.Alignment.CENTER);
-        layout.getStyle().set("margin", "0 auto");
+        HorizontalLayout layout = new HorizontalLayout();
+        layout.setWidthFull();
+        layout.setJustifyContentMode(JustifyContentMode.CENTER);
 
         loginForm = new LoginForm();
-        loginForm.setData(new User());
+        loginForm.setAction("login");
+        loginForm.setForgotPasswordButtonVisible(false);
 
-        Button submit = new Button("Sign in");
-        submit.setIcon(new Icon(VaadinIcon.SIGN_IN));
-        submit.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
-        submit.addClickListener(this::onSubmit);
+        layout.add(loginForm);
 
-        layout.add(
-                new H2("Login"),
-                loginForm,
-                submit
-        );
-
-        setDrawerOpened(false);
-        setContent(layout);
-    }
-
-    private void onSubmit(ClickEvent<Button> buttonClickEvent) {
-        System.out.println(loginForm.getData());
+        add(layout);
     }
 }
--- src/main/java/com/zakat/air_tickets/view/RegisterView.java
+++ src/main/java/com/zakat/air_tickets/view/RegisterView.java
@@ -7,40 +7,49 @@ import com.vaadin.flow.component.html.H2;
 import com.vaadin.flow.component.icon.Icon;
 import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.orderedlayout.FlexComponent;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
+import com.vaadin.flow.router.RouteAlias;
 import com.vaadin.flow.server.auth.AnonymousAllowed;
 import com.zakat.air_tickets.components.HeaderLayout;
 import com.zakat.air_tickets.components.RegisterForm;
+import com.zakat.air_tickets.security.SecurityService;
+import org.springframework.beans.factory.annotation.Autowired;
 
-@Route("/register")
+@Route(value = "/register", layout = HeaderLayout.class)
+@RouteAlias("register")
 @PageTitle("SkyWing | Register")
 @AnonymousAllowed
-public class RegisterView extends HeaderLayout {
+public class RegisterView extends VerticalLayout {
     private final RegisterForm registerForm;
-    private final Button submitButton;
+    private final SecurityService securityService;
 
-    public RegisterView() {
+    public RegisterView(@Autowired SecurityService securityService) {
+        this.securityService = securityService;
         VerticalLayout layout = new VerticalLayout();
         layout.setWidth("40%");
-        layout.setAlignItems(FlexComponent.Alignment.CENTER);
-        layout.getStyle().set("margin", "0 auto");
+        layout.setAlignItems(Alignment.CENTER);
+
+        setAlignItems(Alignment.CENTER);
 
         registerForm = new RegisterForm();
         registerForm.setData(new RegisterForm.RegisterFormData());
 
-        submitButton = new Button("Register");
-        submitButton.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
-        submitButton.setIcon(new Icon(VaadinIcon.SIGN_IN));
-        submitButton.setWidthFull();
-        submitButton.addClickListener(this::onSubmitClick);
+        Button submitBtn = new Button("Register");
+        submitBtn.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+        submitBtn.setIcon(VaadinIcon.SIGN_IN.create());
+        submitBtn.addClickListener(this::onSubmitClick);
 
-        layout.add(new H2("Register"), registerForm, submitButton);
-        setContent(layout);
+        layout.setAlignSelf(Alignment.START, submitBtn);
+        layout.add(new H2("Register"), registerForm, submitBtn);
+        add(layout);
     }
 
     public void onSubmitClick(ClickEvent<Button> buttonClickEvent) {
-        System.out.println(registerForm.getData());
+        RegisterForm.RegisterFormData data = registerForm.getData();
+        securityService.createUser(data.getUsername(), data.getEmail(), data.getPassword());
+        getUI().ifPresent(ui -> ui.navigate("login"));
     }
 }
