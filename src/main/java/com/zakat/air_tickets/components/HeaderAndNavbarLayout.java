ab6274ac866243f04b393144fc364d45ff8f580c
Igor <zakatov0303@gmail.com>
2023-04-16 15:32:09 +0300

    Changed basic layouts
    Added form buttons to HeaderLayout
    Added logout to HeaderAndNavbarLayout

--- src/main/java/com/zakat/air_tickets/components/HeaderAndNavbarLayout.java
+++ src/main/java/com/zakat/air_tickets/components/HeaderAndNavbarLayout.java
@@ -1,35 +1,61 @@
 package com.zakat.air_tickets.components;
 
-import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.applayout.AppLayout;
 import com.vaadin.flow.component.applayout.DrawerToggle;
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.html.H2;
 import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.tabs.Tab;
 import com.vaadin.flow.component.tabs.Tabs;
+import com.vaadin.flow.theme.lumo.LumoUtility;
+import com.zakat.air_tickets.security.SecurityService;
+import org.springframework.security.core.userdetails.UserDetails;
 
-public class HeaderAndNavbarLayout extends HeaderLayout {
+public class HeaderAndNavbarLayout extends AppLayout {
+    private final SecurityService securityService;
+    private final UserDetails user;
 
-    public HeaderAndNavbarLayout() {
+    public HeaderAndNavbarLayout(SecurityService securityService) {
+        this.securityService = securityService;
+        user = securityService.getAuthenticatedUser();
+
         DrawerToggle drawerToggle = new DrawerToggle();
+        H1 title = new H1("SkyWing");
+        title.addClassNames(
+                LumoUtility.FontSize.LARGE,
+                LumoUtility.Margin.NONE
+        );
 
-        title.getStyle()
-                .set("font-size", "var(--lumo-font-size-l)")
-                .set("margin", "0");
+        H2 username = new H2(user.getUsername());
+        username.addClassNames(
+                LumoUtility.Margin.Horizontal.AUTO,
+                LumoUtility.Margin.Vertical.MEDIUM
+        );
 
-        addToDrawer(getTabs());
+        Tabs tabs = getTabs();
+
+        addToDrawer(username, tabs);
         addToNavbar(drawerToggle, title);
 
         setPrimarySection(Section.DRAWER);
     }
 
-    private Component getTabs() {
+    private Tabs getTabs() {
         Tabs tabs = new Tabs();
         tabs.setOrientation(Tabs.Orientation.VERTICAL);
         Tab profile = new Tab(VaadinIcon.USER.create(), new Span("Profile"));
         Tab home = new Tab(VaadinIcon.HOME.create(), new Span("Home"));
         Tab logout = new Tab(VaadinIcon.SIGN_OUT.create(), new Span("Log out"));
 
-        tabs.add(profile, home, logout);
+        tabs.addSelectedChangeListener(e -> {
+            if (e.getSelectedTab() == logout) {
+                securityService.logout();
+            }
+        });
+
+        tabs.add(home, profile, logout);
 
         return tabs;
     }
--- src/main/java/com/zakat/air_tickets/components/HeaderLayout.java
+++ src/main/java/com/zakat/air_tickets/components/HeaderLayout.java
@@ -1,17 +1,34 @@
 package com.zakat.air_tickets.components;
 
 import com.vaadin.flow.component.applayout.AppLayout;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.orderedlayout.FlexComponent;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.theme.lumo.LumoUtility;
 
 public class HeaderLayout extends AppLayout {
-    protected final H1 title;
 
     public HeaderLayout() {
-        title = new H1("SkyWing");
-        title.getStyle()
-                .set("font-size", "var(--lumo-font-size-l)")
-                .set("margin-left", "10%");
+        HorizontalLayout layout = new HorizontalLayout();
+        layout.setJustifyContentMode(FlexComponent.JustifyContentMode.AROUND);
+        layout.setAlignItems(FlexComponent.Alignment.CENTER);
+        layout.setWidthFull();
 
-        addToNavbar(title);
+        H1 title = new H1("SkyWing");
+        title.addClassNames(
+                LumoUtility.FontSize.LARGE
+        );
+
+        Button registerBtn = new Button("Register");
+        registerBtn.addClickListener(e -> this.getUI().ifPresent(ui -> ui.navigate("register")));
+
+        Button loginBtn = new Button("Login");
+        loginBtn.addClickListener(e -> this.getUI().ifPresent(ui -> ui.navigate("login")));
+
+        layout.add(title, new HorizontalLayout(registerBtn, loginBtn));
+
+        addToNavbar(layout);
     }
 }
