a9af66001e220f997a830ecd8401f34b10a9601f
Igor <zakatov0303@gmail.com>
2023-04-20 19:02:54 +0300

    Changed FlightRepository
    Created getAllDepartureCities and getAllArrivalCities
    Fixed search method

--- src/main/java/com/zakat/air_tickets/repository/FlightRepository.java
+++ src/main/java/com/zakat/air_tickets/repository/FlightRepository.java
@@ -2,12 +2,24 @@ package com.zakat.air_tickets.repository;
 
 import com.zakat.air_tickets.entity.Flight;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.sql.Timestamp;
 import java.util.List;
+import java.util.Set;
 
 public interface FlightRepository extends JpaRepository<Flight, Integer> {
-    List<Flight> findAllByDepartureCityContainsAndArrivalCityContainsAndDepartureTimeGreaterThanEqualOrArrivalTimeLessThanEqual(
+    @Query(
+            "SELECT f.departureCity FROM Flight f"
+    )
+    Set<String> getAllDepartureCities();
+
+    @Query(
+            "SELECT f.arrivalCity FROM Flight f"
+    )
+    Set<String> getAllArrivalCities();
+
+    List<Flight> findAllByDepartureCityContainsAndArrivalCityContainsAndDepartureTimeAfterAndArrivalTimeBefore(
             String departureCity,
             String arrivalCity,
             Timestamp departureTime,
@@ -20,7 +32,7 @@ public interface FlightRepository extends JpaRepository<Flight, Integer> {
             Timestamp departureTime,
             Timestamp arrivalTime
     ) {
-        return findAllByDepartureCityContainsAndArrivalCityContainsAndDepartureTimeGreaterThanEqualOrArrivalTimeLessThanEqual(
+        return findAllByDepartureCityContainsAndArrivalCityContainsAndDepartureTimeAfterAndArrivalTimeBefore(
                 departureCity,
                 arrivalCity,
                 departureTime,
